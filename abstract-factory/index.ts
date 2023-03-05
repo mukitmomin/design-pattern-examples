029301425ae82d128869151f5ec0bf0a67e938e8
Mukit Momin <mukit.bin.momin@gmail.com>
2023-03-05 18:00:57 -0500

    added more descriptions, fixed package.json

--- abstract-factory/index.ts
+++ abstract-factory/index.ts
@@ -9,18 +9,33 @@ import { ModernFurnitureFactory } from "./modern-funiture/modern-furniture-facto
  * @param factory {AbstractFurnitureFactory}
  */
 function client(factory: AbstractFurnitureFactory) {
+    // note: the type for chair is `AbstractChair`
+    // this is because the client isn't aware, nor does it care
+    // about the concrete type of chair. this is why a concrete chair 
+    // object MUST implement the AbstractChair interface.
     const chair = factory.createChair();
     const table = factory.createTable();
 
+    // All this works because abstract objects can't be instantiated 
+    // directly. Only objects that implements an abstract interface (concrete)
+    // can be instantiated. Hence the following method calls are guaranteed to 
+    // work without the client needing to be aware of the create type for chair or table
     console.log(chair.sitOn());
     console.log(table.sitOn());
     console.log(table.putFoodOn());
 }
 
 
+/**
+ * the client just wants any object, that implements the abstract 
+ * factory interface. As this concrete object must implement the 
+ * required methods, it has the creation methods
+ */
+console.log("------Modern Factory Client--------");
 const modernFactory = new ModernFurnitureFactory();
 client(modernFactory);
 
 
+console.log("------Art Deco Client--------");
 const artDecoFactory = new ArtDecoFurnitureFactory();
 client(artDecoFactory)
--- abstract-factory/modern-funiture/modern-furniture-factory.ts
+++ abstract-factory/modern-funiture/modern-furniture-factory.ts
@@ -2,13 +2,14 @@ import { AbstractChair } from "../interfaces/abstract-chair";
 import { AbstractFurnitureFactory } from "../interfaces/abstract-factory";
 import { AbstractTable } from "../interfaces/abstract-table";
 import { ModernChair } from "./modern-chair";
+import { ModernTable } from "./modern-table";
 
 export class ModernFurnitureFactory implements AbstractFurnitureFactory {
     createChair(): AbstractChair {
         return new ModernChair();
     }
     createTable(): AbstractTable {
-        throw new Error("Method not implemented.");
+        return new ModernTable();
     }
 
 }
